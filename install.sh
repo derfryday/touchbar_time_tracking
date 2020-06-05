7e6896c211daea3dffd2c0052e0de01ca0ede488
Erik Freitag <erik.freitag@adup-tech.com>
2020-06-05 07:58:02 +0200

    items.json added missing /; install.json fixed copy location string

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ cp items.json.example items.json
 sed -i "" "s#<USERNAME>#$USERNAME#g" items.json
 sed -i "" "s#<PASSWORD>#$PASSWORD#g" items.json
 sed -i "" "s#<STATE_PATH>#$STATE_PATH#g" items.json
-cp items.json $HOME/Library/Application Support/MTMR/
+cp items.json "$HOME/Library/Application Support/MTMR/"
 echo "pause" > "$STATE_PATH/smalltime.state"
 echo "install complete"
